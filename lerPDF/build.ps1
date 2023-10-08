1cbdd82219901260d2f27ca993e098658d1bae8a
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-08 19:14:59 -0300

    Criando o arquivo sqlite3

--- lerPDF/build.ps1
+++
@@ -1,3 +0,0 @@
-$exclude = @("venv", "lerPDF.zip")
-$files = Get-ChildItem -Path . -Exclude $exclude
-Compress-Archive -Path $files -DestinationPath "lerPDF.zip" -Force
--- lerPDF/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-zip -r "lerPDF.zip" * -x "lerPDF.zip"
