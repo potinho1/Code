02a55238b2e73b5cd6b436cf848dda8916c86abd
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-16 16:38:14 -0300

    Tipo str (string) - Introdução aos tipos de dados

---
+++ CursoUdemyPython/Python/aula3/main.py
@@ -0,0 +1,17 @@
+"""
+Python = Linguagem de programação 
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+# Aspas simples
+print('Potinho')
+
+# Aspas duplas
+print("Potinho")
+
+# Escape
+print("Potinho\"João\"")
+
+# r
+print(r"Potinho\"João\"")
