a754540197d39518a3c109db3cb615471952d1fb
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-17 04:40:46 -0300

    Introdução às variáveis em Python

---
+++ CursoUdemyPython/Python/aula7/main.py
@@ -0,0 +1,16 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar números e underline_.
+# O sinal de = é o operador de atribuição. Ele é usado para atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+nome_completo = 'João Vitor'
+soma_dois_mais_dois = 2 + 2
+int_um = int('1')
+print(int_um, type(int_um))
+print(nome_completo, soma_dois_mais_dois)
+
+
+nome = 'Potinho'
+idade = 22
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
