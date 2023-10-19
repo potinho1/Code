72d05f45c69705c124288c8868567d72e65ae336
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-19 04:54:31 -0300

    CREATE TABLE para criar tabela com PRIMARY KEY no PyMySQL

--- CursoUdemyPython/aula206/main.py
+++ CursoUdemyPython/aula206/main.py
@@ -13,5 +13,13 @@ connection = pymysql.connect(
 
 with connection:
     with connection.cursor() as cursor:
-      #SQL
+      cursor.execute(
+        'CREATE TABLE ID NOT EXISTS customers ('
+        'id INT NOT NULL AUTO_INCREMENT, '
+        'nome VARCHAR(50) NOT NULL, '
+        'idade INT NOT NULL, '
+        'PRIMARY KEY (id)'
+        ') '
+      )
+      connection.commit()
       print(cursor)
