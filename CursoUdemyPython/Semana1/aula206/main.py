b04be0bd7f6f28b8e6c35a41b0e750d6783e638f
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-22 09:43:00 -0300

    Revisando

--- CursoUdemyPython/Semana1/aula206/main.py
+++ CursoUdemyPython/Semana1/aula206/main.py
@@ -2,8 +2,10 @@ import pymysql
 import dotenv
 import os
 
+# Carrega variaveis de ambiente 
 dotenv.load_dotenv()
 
+# Conecta no banco de dadaos e usa as credenciais de=o arquivo env
 connection = pymysql.connect(
     host=os.environ['MYSQL_HOST'],
     user=os.environ['MYSQL_USER'],
@@ -11,8 +13,12 @@ connection = pymysql.connect(
     database=os.environ['MYSQL_DATABASE'],
 )
 
+# Criando a tabela 'customers' se ela não existir
+# conexao com o banco
 with connection:
+    # Criando a função cursor
     with connection.cursor() as cursor:
+      # Executa a criação da tabela, se ela não existir
       cursor.execute(
         'CREATE TABLE ID NOT EXISTS customers ('
         'id INT NOT NULL AUTO_INCREMENT, '
@@ -21,5 +27,6 @@ with connection:
         'PRIMARY KEY (id)'
         ') '
       )
+      # Confirma as alterações feitas no banco
       connection.commit()
       print(cursor)
