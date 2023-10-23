e8611e5b896f0d67cdf8c2f564e26419500c9478
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-23 18:58:04 -0300

    Exercício IMC

---
+++ CursoUdemyPython/Semana2/Python/ExercicioAula12.py
@@ -0,0 +1,10 @@
+# Calculando IMC
+
+nome = 'João Vitor'
+altura = 1.80
+peso = 60
+imc = peso / (altura * altura)
+
+print(f'{nome} tem {altura} de altura,')
+print(f'pesa {peso} quilos e sei IMC é')
+print(imc)
