34eff99e5e5fb953cb6eab022f9f28cebe70b8a0
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-23 19:02:11 -0300

    Exercício IMC

--- CursoUdemyPython/Semana2/Python/ExercicioAula12.py
+++ CursoUdemyPython/Semana2/Python/ExercicioAula12.py
@@ -5,6 +5,6 @@ altura = 1.80
 peso = 60
 imc = peso / (altura * altura)
 
-print(f'{nome} tem {altura} de altura,')
+print(f'{nome} tem {altura:,.2f} de altura,')
 print(f'pesa {peso} quilos e sei IMC é')
 print(imc)
