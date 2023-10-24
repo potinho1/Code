ca5eabe96351cc032beafc6205ccee7024a42585
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-24 04:47:28 -0300

    Usando a função input para coletar dados do usuário

---
+++ CursoUdemyPython/Semana2/Python/aula15.py
@@ -0,0 +1,10 @@
+nome = input('Qual o seu nome? ') # Todo input retorna uma string = str
+print(f'O seu nome é: {nome=}') # O '=' serve para verificar o nome e o valor da variavel
+
+n1 = input('Digite um número: ')
+n2 = input('Digite outro número: ')
+
+cover_n1 = int(n1)
+cover_n2 = int(n2)
+
+print(f'A soma dos núemros é: {cover_n1 + cover_n2}')
