58cb2888a72ad945482610e802a33a1bc11d2595
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-10-30 19:04:26 -0300

    Solução - Exercício de programação com if e comparação

--- CursoUdemyPython/Semana3/Python/Exercicio1.py
+++ CursoUdemyPython/Semana3/Python/Exercicio1.py
@@ -8,3 +8,22 @@ elif segundo_valor > primeiro_valor:
     print(f"{segundo_valor=} é maior do que {primeiro_valor=}")
 else:
     print("Valor incorreto")
+
+
+# Solução do professor
+
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor >= segundo_valor:
+    print(
+        f'{primeiro_valor=} é maior ou igual '
+        f'ao que {segundo_valor=}'
+    )
+else:
+    print(
+        f'{segundo_valor=} é maior '
+        f'do que {primeiro_valor=}'
+    )
+
+# Lembrado estava igual o meu, porém ele adicionou o >=
