7cbdfbe85500ffac2795a6bb111bad39a66012c9
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-11-28 04:32:11 -0300

    Operadores in e not in

---
+++ CursoUdemyPython/Semana4/Aula24.py
@@ -0,0 +1,12 @@
+# Operadores in e not in 
+# Strings são intráveis 
+#  0 1 2 3 
+# J O Ã O 
+# -3-2-1-0
+
+nome = 'João'
+print(nome[2])
+print(nome[-2])
+
+print('J' in nome)
+print('o' not in nome)
