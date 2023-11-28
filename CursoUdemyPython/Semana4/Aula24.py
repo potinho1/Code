035ca4e553cacf9d1a5b590460f30c73d0886b47
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-11-28 04:36:52 -0300

    Operadores in e not in

--- CursoUdemyPython/Semana4/Aula24.py
+++ CursoUdemyPython/Semana4/Aula24.py
@@ -10,3 +10,15 @@ print(nome[-2])
 
 print('J' in nome)
 print('o' not in nome)
+
+
+print(10 * '-')
+
+
+usuario = input("Digite seu nome: ")
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in usuario:
+    print(f"{encontrar} está em {usuario}")
+else:
+    print(f"{encontrar} não está em {usuario}")
