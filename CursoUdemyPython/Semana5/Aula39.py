01a6ed9d1fe50951e03803da79390a5c30ec8573
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-12-05 06:17:06 -0300

    Resolução do exercicio

--- CursoUdemyPython/Semana5/Aula39.py
+++ CursoUdemyPython/Semana5/Aula39.py
@@ -4,13 +4,14 @@ Iterando strings com while
 """
 
 nome = "Potinho" # Iteráveis
-tamanho_nome = len(nome)
-print(tamanho_nome)
 iterar = 0
 
-while iterar <= tamanho_nome:
+while iterar < len(nome):
+    iterador = nome[iterar]
+
+    print(f"*{iterador}")
+
     iterar += 1
-    #Resolver o teste
-    # Preciso desse resultado *P*o*t*i*n*h*o*
+    
 
     
