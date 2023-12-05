c4d990440356f929e7f1441d55a08acabd210278
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-12-05 05:33:47 -0300

    Resolver o teste

---
+++ CursoUdemyPython/Semana5/Aula39.py
@@ -0,0 +1,16 @@
+"""
+Iterando strings com while
+
+"""
+
+nome = "Potinho" # Iteráveis
+tamanho_nome = len(nome)
+print(tamanho_nome)
+iterar = 0
+
+while iterar <= tamanho_nome:
+    iterar += 1
+    #Resolver o teste
+    # Preciso desse resultado *P*o*t*i*n*h*o*
+
+    
