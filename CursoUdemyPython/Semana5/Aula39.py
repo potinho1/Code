8b0b759d06c54491ce11732d885b12ca87ab6d10
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-12-06 04:54:30 -0300

    Ajustando o exercicio

--- CursoUdemyPython/Semana5/Aula39.py
+++ CursoUdemyPython/Semana5/Aula39.py
@@ -5,13 +5,17 @@ Iterando strings com while
 
 nome = "Potinho" # Iteráveis
 iterar = 0
+novo_nome = ""
 
 while iterar < len(nome):
     iterador = nome[iterar]
-
-    print(f"*{iterador}")
+    letra = f"*{iterador}"
+    novo_nome += letra
 
     iterar += 1
+
+
+print(novo_nome)
     
 
     
