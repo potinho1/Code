92dd24e6b2ec3deea6e39ce50da997e1baeef682
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-12-12 05:25:11 -0300

    Solução do professor

--- CursoUdemyPython/Semana6/ExercicioLista.py
+++ CursoUdemyPython/Semana6/ExercicioLista.py
@@ -1,6 +1,26 @@
+"""
+Exercício
+Exiba os índices da lista
+0 Maria
+1 Helena
+2 Luiz
+"""
+
 lista = ["Maria", "Helena", "Luiz", "Maria", "Helena", "Luiz"]
 indice = 0
 
 for nome in lista:
     print(f"{indice} - {nome}")
     indice += 1
+
+
+# Jeito que o professor fez
+
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('João')
+
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
