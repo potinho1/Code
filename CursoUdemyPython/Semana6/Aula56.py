744ec634b2b336feaba7aa24812bad15d41e7742
joao vitor gomes <gomesjoaovitor222@gmail.com>
2023-12-14 05:29:10 -0300

    Listas dentro de listas (iteráveis dentro de iteráveis)

--- CursoUdemyPython/Semana6/Aula56.py
+++ CursoUdemyPython/Semana6/Aula56.py
@@ -7,10 +7,14 @@ salas = [
     # 0
     ['Elaine', ],  # 1
     # 0       1       2
-    ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)],  # 2
+    # ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)],  # 2
+    ['Luiz', 'João', 'Eduarda']
 ]
 
-print(salas[0][1])
-print(salas[2][2])
-print(salas[2][3][2])
+# print(salas[0][1])
+# print(salas[2][2])
+# print(salas[2][3][2])
 
+for sala in salas:
+    for aluno in sala:
+        print(aluno)
