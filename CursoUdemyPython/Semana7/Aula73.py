b644fbcf770c4874795f54fab02edd2e6f01ec52
joao vitor gomes <gomesjoaovitor222@gmail.com>
2024-03-03 15:21:08 -0300

    Termos técnicos

--- CursoUdemyPython/Semana7/Aula73.py
+++ CursoUdemyPython/Semana7/Aula73.py
@@ -8,3 +8,17 @@ def executa(funcao, *args):
 
 v = executa(saudacao, "Olá Mundo!", "Potinho", 23)
 print(v)
+
+
+"""
+Termos técnicos: Higher Order Functions e First-Class Functions
+Academicamente, os termos Higher Order Functions e First-Class Functions têm significados diferentes.
+
+Higher Order Functions - Funções que podem receber e/ou retornar outras funções
+
+First-Class Functions - Funções que são tratadas como outros tipos de dados comuns (strings, inteiros, etc...)
+
+Não faria muita diferença no seu código, mas penso que deveria lhe informar isso.
+
+Observação: esses termos podem ser diferentes e ainda refletir o mesmo significado.
+"""
