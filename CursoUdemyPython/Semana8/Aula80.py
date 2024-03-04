e979dabfd78ae0f508a99dedf17dca2fb79bad68
joao vitor gomes <gomesjoaovitor222@gmail.com>
2024-03-04 19:36:03 -0300

    set part 2

--- CursoUdemyPython/Semana8/Aula80.py
+++ CursoUdemyPython/Semana8/Aula80.py
@@ -19,6 +19,14 @@ s1 = {'Luiz', 1, 2, 3}  # com dados
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
 
+# l1 = [1, 2, 3, 3, 3, 3, 3, 1]
+# s1 = set(l1)
+# l2 = list(s1)
+s1 = {1, 2, 3}
+print(3 not in s1)
+for numero in s1:
+    print(numero)
+
 # Métodos úteis:
 # add, update, clear, discard
 
