1857076c734081364d4e97d5cb80afe556f5bcea
joao vitor gomes <gomesjoaovitor222@gmail.com>
2024-03-04 09:53:47 -0300

    get,pop e update

--- CursoUdemyPython/Semana8/Aula78.py
+++ CursoUdemyPython/Semana8/Aula78.py
@@ -10,8 +10,8 @@
 # popitem - Apaga o último item adicionado
 # update - Atualiza um dicionário com outro
 pessoa = {
-    'nome': 'Luiz Otávio',
-    'sobrenome': 'Miranda',
+    'nome': 'João Vitor',
+    'sobrenome': 'Gomes',
     'idade': 900,
 }
 
@@ -27,3 +27,42 @@ print(pessoa['idade'])
 
 # for chave, valor in pessoa.items():
 #     print(chave, valor)
+
+# Copy e Shallow copy
+
+import copy
+
+d1 = {
+    'c1': 1,
+    'c2': 2,
+    'l1': [0, 1, 2],
+}
+d2 = d1.copy()
+
+d2['c1'] = 1000
+d2['l1'][1] = 999999
+
+print(d1)
+print(d2)
+
+
+# get é pode colocar um frase se ele não encontrar
+
+print(pessoa.get('nome', 'None'))
+
+# pop ele remove o item
+
+pessoa.pop("nome")
+print(pessoa)
+
+# popoitem ele remove o ultimo item na lista
+
+pessoa.popitem()
+print(pessoa)
+
+# update ele atualiza uma chave e pode criar uma nova chave
+
+lista = [['robs', 'Dev'], ['Teste', '1234']]
+pessoa.update(lista)
+
+print(pessoa)
