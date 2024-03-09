f14d01f70caad2be62295b05e9f9eda4f441fe83
joao vitor gomes <gomesjoaovitor222@gmail.com>
2024-03-09 17:08:57 -0300

    Estudando set

--- CursoUdemyPython/Semana8/Aula80.py
+++ CursoUdemyPython/Semana8/Aula80.py
@@ -29,9 +29,27 @@ for numero in s1:
 
 # Métodos úteis:
 # add, update, clear, discard
+    
+s1 = set()
+s1.add("Potinho")
+s1.add(1)
+s1.update(("Vitor", 1,2,3,4))
+# s1.clear()
+print(s1)
+s1.discard("Potinho")
+print(s1)
+
 
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+
+s1 = {1,2,3}
+s2 = {2,3,4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s1 - s2
+s3 = s1 ^ s2
+print(s3)
