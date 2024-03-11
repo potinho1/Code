a6c1367ceb72cf89d86ccd7742c9a7f38ece3da1
joao vitor gomes <gomesjoaovitor222@gmail.com>
2024-03-11 14:07:28 -0300

    Mapeamento de dados em list comprehension (map)

---
+++ CursoUdemyPython/Semana9/Aula86.py
@@ -0,0 +1,19 @@
+lista = [num for num in range(10)]
+lista2 = [num * 2 for num in range(10)]
+
+print(lista)
+print(lista2)
+
+
+produtos = [
+    {"nome": "p1","preco": 20, },
+    {"nome": "p2","preco": 10, },
+    {"nome": "p3","preco": 30, },]
+
+novos_produtos =[
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+]
+
+print(*novos_produtos, sep="\n")
